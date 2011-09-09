806910780c3db8bbd555694c8058cf4e64c36b07
Lee Smith <jeremyleesmith@gmail.com>
2011-09-08 22:21:45 -0500

    Rails 3.1

---
+++ Gemfile
@@ -0,0 +1,16 @@
+source 'http://rubygems.org'
+
+gem 'pg'
+gem 'rails', '3.1.0'
+
+# Gems used only for assets and not required
+# in production environments by default.
+group :assets do
+  gem 'sass-rails', "  ~> 3.1.0"
+  gem 'coffee-rails', "~> 3.1.0"
+  gem 'uglifier'
+end
+
+group :development, :test do
+  gem 'ruby-debug19', require: 'ruby-debug'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TicketMule::Application.config.secret_token = 'b967e33a5ec287578b6ef5d75aa95d56600d0cd2c1101a6bd32fddad96d4f3b8a0a926ce2d791509f177d90120fa16e956da476d272be5cd161993c81fa69012'
